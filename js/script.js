f780a4de37abbfbffddbeea87fa0f1a8fc420bc6
Viktor1111 <burlu@bk.ru>
2017-02-20 17:21:01 +0500

    исправления после проверки

--- js/script.js
+++ js/script.js
@@ -11,3 +11,19 @@
         event.preventDefault();
         popup.classList.remove("modal-content-show");
       });
+
+.friends-logo::before {
+    position: absolute;
+    top: 31px;
+    content: url(../img/divide.png);
+}
+.friends-logo::after {
+    position: absolute;
+    top: 214px;
+    content: url(../img/divide.png);
+}
+.column:before {
+    position: absolute;
+    top: 63px;
+    content: url(../img/divide.png);
+}
